218ccfe7d5aa2b3fcffeb30aad9d3f8e1bcee1dd
ibrahimKadri <78695030+ibrahimKadri@users.noreply.github.com>
2022-02-07 19:14:49 +0200

    Add files via upload

---
+++ Portfolio/js/type.js
@@ -0,0 +1,6 @@
+var typed = new Typed('.typing-text', {
+    // Waits 1000ms after typing "First"
+    strings: ['web designer','front-end developer','photographer','graphic designer'],
+    typeSpeed:120,
+    loop:true,
+  });
